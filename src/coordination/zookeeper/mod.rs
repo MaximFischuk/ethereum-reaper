f7898c0310f0781040dc3a488b91bb2abaa45bc9
MaximFischuk <mfischuk@vareger.com>
2020-05-18 07:22:00 +0300

    LoggingWatcher is public now

--- src/coordination/zookeeper/mod.rs
+++ src/coordination/zookeeper/mod.rs
@@ -3,7 +3,7 @@ use zookeeper::{Watcher, WatchedEvent};
 pub mod distributed_number;
 pub mod interprocess_lock;
 
-struct LoggingWatcher;
+pub struct LoggingWatcher;
 
 impl Watcher for LoggingWatcher {
     fn handle(&self, e: WatchedEvent) {
